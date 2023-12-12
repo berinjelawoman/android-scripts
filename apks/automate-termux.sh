eaae35f31a1e15ec75607e31bf3dd4004be25639
Berin Jela <berinjelawoman@gmail.com>
2023-12-12 17:21:27 -0300

    add intelbras v2box support

--- apks/automate-termux.sh
+++ apks/automate-termux.sh
@@ -1,17 +1,35 @@
 IP=$1
 ERROR_THRESH=10
+SCREENCAP_FILE=screencap$IP.png
+SCREENCAP_FILE_INTELBRAS=screencapintelbras$IP.png
 
 adb -s $IP shell monkey -p com.termux 1
+
 
 # wait for termux initial setup
 echo Waiting for initial setup
 while true; do 
-    adb -s $IP exec-out screencap -p > screencap.png
+    adb -s $IP shell 'screencap -p /sdcard/screencap.png'
+    adb -s $IP pull /sdcard/screencap.png $SCREENCAP_FILE
+    cp $SCREENCAP_FILE $SCREENCAP_FILE_INTELBRAS
+
+    convert $SCREENCAP_FILE -crop x500+0+0 $SCREENCAP_FILE
     diff=$(compare -channel red -metric MSE \
-        termux-home.png screencap.png out.png 2>&1 | cut -d' ' -f1)
+        termux-home.png $SCREENCAP_FILE out.png 2>&1 | cut -d' ' -f1)
+    diff="${diff%.*}"
     if (( $(echo "$diff < $ERROR_THRESH" | bc -l) )); then
         break
     fi
+
+    convert $SCREENCAP_FILE_INTELBRAS -crop x400+0+0 $SCREENCAP_FILE_INTELBRAS
+    diff=$(compare -channel red -metric MSE \
+        termux-home-intelbras.png $SCREENCAP_FILE_INTELBRAS out.png 2>&1 | cut -d' ' -f1)
+    diff="${diff%.*}"
+    if (( $(echo "$diff < $ERROR_THRESH" | bc -l) )); then
+        break
+    fi
+
+
     sleep 1
 done
 rm out.png
@@ -21,6 +39,8 @@ echo Running install commands
 read -r -d '' CMDS << EOM
  input text termux-change-repo && \
     for i in $(seq -s ' ' 1 3); do input keyevent KEYCODE_ENTER; done && \
+    input text "echo%s'allow-external-apps=true'>>~/.termux/termux.properties" && \
+    input keyevent KEYCODE_ENTER && \
     input text "pkg%sinstall%sandroid-tools%s-y&&adb%sdevices" && \
     input keyevent KEYCODE_ENTER
 EOM
@@ -29,16 +49,28 @@ adb -s $IP shell "$CMDS"
 
 # check for confirmation box
 echo Waiting for confirmation box
+adb -s $IP shell input keyevent KEYCODE_HOME
 while true; do
-    adb -s $IP exec-out screencap -p > screencap.png
-    convert screencap.png -crop 600x300+500+350 screencap.png
-    res=$(tesseract screencap.png stdout)
-    if [[ "$res" == *"chave RSA"* ]]; then
+    adb -s $IP shell 'screencap -p /sdcard/screencap.png'
+    adb -s $IP pull /sdcard/screencap.png $SCREENCAP_FILE
+    cp $SCREENCAP_FILE $SCREENCAP_FILE_INTELBRAS
+
+    convert $SCREENCAP_FILE -crop 600x300+500+350 $SCREENCAP_FILE
+    res=$(tesseract $SCREENCAP_FILE stdout)
+    if [[ "$res" == *"chave RSA"* || "$res" == *"RSA key"* ]]; then
         break
     fi
+
+    convert $SCREENCAP_FILE_INTELBRAS -crop 600x300+350+150 $SCREENCAP_FILE_INTELBRAS
+    res=$(tesseract $SCREENCAP_FILE_INTELBRAS stdout)
+    if [[ "$res" == *"chave RSA"* || "$res" == *"RSA key"* ]]; then
+        break
+    fi
+
     sleep 1
 done
-rm screencap.png
+rm $SCREENCAP_FILE
+rm $SCREENCAP_FILE_INTELBRAS
 
 sleep 1
 adb -s $IP shell input keyevent KEYCODE_ENTER
